6a600e6970ae4dbe62117cfb3a93bfb83114d1b9
KovOlga <switch.to.olga@yandex.ru>
2024-12-11 22:39:53 +0300

    added btns components

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -1,8 +1,9 @@
 import { ReactElement } from 'react';
 import Header from '../header';
-import TodosList from '../todosList';
 import './styles.css';
 import Input from '../input';
+import TodoList from '../todoList';
+import { Button, ButtonGroup, Chip } from '@mui/material';
 
 function App(): ReactElement {
   return (
@@ -10,7 +11,20 @@ function App(): ReactElement {
       <Header />
       <main className="wrapper">
         <Input />
-        <TodosList />
+        <TodoList />
+        <div className="controls">
+          <Chip label="2 left" size="small" variant="outlined" />
+          <ButtonGroup variant="outlined" aria-label="Basic button group">
+            <Button size="small" variant="contained">
+              One
+            </Button>
+            <Button size="small">Two</Button>
+            <Button size="small">Three</Button>
+          </ButtonGroup>
+          <Button variant="outlined" size="small">
+            Clear completed
+          </Button>
+        </div>
       </main>
     </div>
   );
--- src/components/todoList/index.tsx
+++ src/components/todoList/index.tsx
@@ -2,7 +2,7 @@ import { ReactElement } from 'react';
 import { useAppSelector } from '../../store/hooks';
 import { Checkbox, Divider, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
 
-export default function TodosList(): ReactElement {
+export default function TodoList(): ReactElement {
   const todos = useAppSelector((state) => state.todos.todos);
   return (
     <List>
@@ -10,9 +10,9 @@ export default function TodosList(): ReactElement {
         return (
           <>
             <ListItem disablePadding key={item.id}>
-              <ListItemButton role={undefined} dense>
+              <ListItemButton dense>
                 <ListItemIcon>
-                  <Checkbox edge="start" disableRipple checked={item.complete} tabIndex={-1} />
+                  <Checkbox edge="start" color="success" disableRipple checked={item.complete} tabIndex={-1} />
                 </ListItemIcon>
                 <ListItemText id={item.id.toString()} primary={item.name} />
               </ListItemButton>
